f09430bf17d5ea2aaf52996b012efde0152da33d
omer-jan <paghmane@yahoo.com>
2018-04-30 11:51:47 +0430

    database file is added

---
+++ phonebook.sql
@@ -0,0 +1,61 @@
+-- Adminer 4.5.0 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1,	'2014_10_12_000000_create_users_table',	1),
+(2,	'2014_10_12_100000_create_password_resets_table',	1),
+(3,	'2018_04_26_080248_create_phonebooks_table',	1);
+
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `phonebooks`;
+CREATE TABLE `phonebooks` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `phonebooks` (`id`, `name`, `phone`, `email`, `created_at`, `updated_at`) VALUES
+(76,	'jan',	2147483647,	'fortest@test.com',	'2018-04-30 01:31:42',	'2018-04-30 01:34:05'),
+(77,	'khan',	2147483647,	'test@test.com',	'2018-04-30 01:33:38',	'2018-04-30 01:33:38');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- 2018-04-30 07:21:02
--- readme.md
+++ readme.md
@@ -1,15 +1,20 @@
-<p align="center"> <img src="/phonebook/homepage.JPG" alt="homepage"  height="42" width="42"></p>
-
- <br>
- <br>
-
-
-## About this Project
+<p align="center">PhoneBook </p>
  
-         this is simple phonebook project is  developed by Mohammad Zubair (omer jan) Software Developer using the following frameworks:<br>
-   ## <a href="https://laravel.com/">Laravel :</a></strong>
-       <p>Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern. for more details about laravel <a href="https://laravel.com/">click here</a>.</p>
+ ## About this Project
+ 
+  this is simple phonebook project is  developed by Mohammad Zubair (omer jan) Software Developer using the following frameworks
 
-    ##<strong><a href="https://vuejs.org/">Vue.js : </a></strong> <p>Vue.js is an open-source JavaScript framework for building user interfaces. it is used for client side  processing. for more details about Vue.js <a href="https://vuejs.org/">click me</a>.</p>
-    
-    ##<strong><a href="https://bulma.io/">Bluma :</a> </strong><p>Bulma is an open source CSS framework based on Flexbox and built with Sass. It's 100% responsive, fully modular, and available for free. for more details about Bluma <a href="https://bulma.io/">click here</a>.</p>
+## Laravel :
+
+Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern. for more details about laravel.for more details about laravel click [ here](https://laravel.com/docs)
+
+## Vue.js :
+
+Vue.js is an open-source JavaScript framework for building user interfaces. it is used for client side  processing. for more details about Vue.js. for more details click [here](https://vuejs.org/).
+
+## Bluma :
+
+ Bulma is an open source CSS framework based on Flexbox and built with Sass. It's 100% responsive, fully modular, and available for free. for more details about Bluma.for more details click [here](https://bulma.io/)
+## License
+
+the source code is free and available to every one
